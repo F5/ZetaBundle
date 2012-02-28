9997ac18bf32ae8ccdc52f2d3af63c13ac47f04d
Francisco Facioni <fran6co@gmail.com>
2012-02-28 16:25:30 -0300

    Using event subscribre instead of listener

--- EventListener/EzcSearchListener.php
+++ EventListener/EzcSearchListener.php
@@ -5,8 +5,10 @@ namespace F5\Bundle\ZetaBundle\EventListener;
 use \ezcSearchSession;
 use \ezcBasePersistable;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Doctrine\Common\EventSubscriber;
+use Doctrine\ORM\Events;
 
-class EzcSearchListener
+class EzcSearchListener implements EventSubscriber
 {
     protected $searchSession;
 
@@ -34,5 +36,19 @@ class EzcSearchListener
         if ($args->getEntity() instanceof ezcBasePersistable) {
             $this->searchSession->deleteById($args->getEntity()->getId(),get_class($args->getEntity()));
         }
+    }
+
+    /**
+     * Returns an array of events this subscriber wants to listen to.
+     *
+     * @return array
+     */
+    function getSubscribedEvents()
+    {
+        return array(
+            Events::postPersist,
+            Events::postUpdate,
+            Events::postRemove,
+        );
     }
 }
